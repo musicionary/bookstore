e581ed53d56f1bd3e73aa9aacabe4ab7a9010492
Chip Carnes <chip.carnes27@gmail.com>
2017-02-24 12:00:08 -0600

    Moved func test to own folder and removed voodoo sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,14 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
+
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -12,13 +16,21 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_grid(self, row_text):
-        grid = self.browser.find_element_by_id('id_bookshelf_grid')
-        rows = grid.find_elements_by_tag_name('h2')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                grid = self.browser.find_element_by_id('id_bookshelf_grid')
+                rows = grid.find_elements_by_tag_name('h2')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_bookshelf_and_retrieve_it_later(self):
         # Checkout the homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # Check the webpage title and header mention the book case
         self.assertIn("BookCase", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -35,14 +47,12 @@ class NewVisitorTest(unittest.TestCase):
         # When hitting enter, the page updates,
         # and the page lists "Want To Read"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         self.check_for_row_in_grid('Want To Read')
 
         # Enters another bookshelf
         inputbox = self.browser.find_element_by_id('id_new_shelf')
         inputbox.send_keys('Finished Books')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         self.check_for_row_in_grid('Finished Books')
 
         self.fail('Finish the test!')
@@ -52,7 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         # there is some explanatory text to that effect.
 
         # Visit the url - the book shelf is still there.
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
